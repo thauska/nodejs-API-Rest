8bf6dc4e5fa6d20d6ae45f98d97ad651c174f126
Thayana Mamoré <thayanacmamore@gmail.com>
2021-03-24 12:22:35 -0300

    API inicializada e configurada

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const config = require('config')
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(config.get('api.port'), () => console.log('API rodando'))
