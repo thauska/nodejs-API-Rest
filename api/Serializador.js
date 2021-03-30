2e29da5d11f6e9757bce903f8392444d0f9abaf7
Thayana Mamoré <thayanacmamore@gmail.com>
2021-03-30 18:02:20 -0300

    Incluir suporte a XML processando no Serializador

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,34 @@
-const ValorNaoSuportado = require("./errors/ValorNaoSuportado")
-
+const ValorNaoSuportado = require('./errors/ValorNaoSuportado')
+const jsontoxml = require('jsontoxml')
 class Serializador {
     json(dados) {
         return JSON.stringify(dados)
     }
 
+    xml (dados) {
+        let tag = this.tagSingular
+
+        if (Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+
+        return jsontoxml({ [tag]: dados })
+    }
+
     serializar(dados) {
+        dados = this.filtrar(dados)
+
         if(this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+
+        if (this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -49,6 +68,8 @@ class SerializadorFornecedor extends Serializador {
             'empresa',
             'categoria'
         ].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -67,5 +88,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
